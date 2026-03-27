8ab9f4487945bffbe1fa959416270c6c515c69c0
Your Name <sovonsutarsutar123@gmail.com>
2026-03-27 17:45:53 +0530

    Add navigation functionality to "Our Team" link in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import logo from "../assets/logo.png";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
-
+  const navigate = useNavigate();
   return (
     <div className="w-full py-4 flex justify-center">
       <div className="bg-black w-[95%] md:w-auto gap-8 rounded-full px-6 md:px-10 py-2 flex items-center justify-between text-white relative">
@@ -17,14 +18,26 @@ const Navbar = () => {
         {/* LEFT NAV (DESKTOP) */}
         <div className="hidden md:flex gap-8 lg:gap-16 items-center">
           <span className="cursor-pointer text-sm">Service</span>
-          <span className="cursor-pointer text-sm">Our Team</span>
+          <span
+            className="cursor-pointer text-sm"
+            onClick={() => {
+              navigate("/our-team");
+            }}
+          >
+            Our Team
+          </span>
           <span className="bg-yellow-400 text-black px-5 py-2 rounded-full font-medium cursor-pointer">
             Home
           </span>
         </div>
 
         {/* LOGO */}
-        <div className="absolute left-1/2 transform -translate-x-1/2 md:static md:transform-none">
+        <div
+          onClick={() => {
+            navigate("/");
+          }}
+          className="absolute cursor-pointer left-1/2 transform -translate-x-1/2 md:static md:transform-none"
+        >
           <img src={logo} alt="logo" className="h-10 md:h-14 object-contain" />
         </div>
 
@@ -39,7 +52,14 @@ const Navbar = () => {
         {open && (
           <div className="absolute top-16 left-0 w-full bg-black rounded-2xl flex flex-col items-center gap-4 py-6 md:hidden z-50">
             <span className="cursor-pointer text-sm">Service</span>
-            <span className="cursor-pointer text-sm">Our Team</span>
+            <span
+              className="cursor-pointer text-sm"
+              onClick={() => {
+                navigate("/our-team");
+              }}
+            >
+              Our Team
+            </span>
             <span className="bg-yellow-400 text-black px-5 py-2 rounded-full font-medium cursor-pointer">
               Home
             </span>
